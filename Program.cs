5598cfb278c9eb98aec7552e6f2279674453a272
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-26 10:38:46 +0530

    [Tirupathi] Add. uC6

--- Program.cs
+++ Program.cs
@@ -18,9 +18,7 @@ namespace usecase2
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 2);
+            
 
             //if (empCheck == IS_FULL_TIME)
             //{
@@ -30,23 +28,31 @@ namespace usecase2
             //{
             //    empHrs = 0;
             //}
-
-            switch (empCheck)
+            int totalEmpHrs = 0;
+            for (int attendance = 0; attendance < 30; attendance++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs = totalEmpHrs + empHrs;
+                if(totalEmpHrs > 100 || attendance >= 20)
+                {
                     break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default :   
-                    empHrs=0;
-                    break;
-
-
+                }
             }
 
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+            empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
 
         }
