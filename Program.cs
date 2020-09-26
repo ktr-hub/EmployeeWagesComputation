f670ec27aede466a96b11c341b3eb39e8cfec7d8
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-26 10:05:23 +0530

    [Tirupathi] Add. uC3

--- Program.cs
+++ Program.cs
@@ -6,33 +6,26 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You are now in Welcome page of Employee Wages");
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
 
-            int IS_FULL_TIME = 1;
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
-
             int empCheck = random.Next(0, 2);
 
-            Console.WriteLine(empCheck);
-
-            if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
-            else
-                Console.WriteLine("Employee is not present");
-
-            int EMP_RATE_PER_HOUR = random.Next(0, 2);
-            int empHrs = 0;
-
-            if (empCheck == IS_FULL_TIME)
+            if(empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
-            else
-            {
-                empHrs = 0;
+            else{
+                empHrs = 4;
             }
-            int empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage UC3 : " + empWage);
 
         }
     }
