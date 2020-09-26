bc21125e2477b84f895e9ea6b87b91690dbe6f49
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-09-26 09:24:13 +0530

    Add. added uc2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 
-namespace EmployeeWageComputation
+namespace usecase2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You are now in Welcome page of Employee Wages");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
 
-            Console.WriteLine(empCheck);
-
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
+            {
+                empHrs = 8;
+            }
             else
-                Console.WriteLine("Employee is not present");
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+
         }
     }
 }
